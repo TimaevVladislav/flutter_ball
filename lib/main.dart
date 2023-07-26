48986500d260b25984c4f9252487de1bb572f414
timaevV <timaevvladislav@gmail.com>
2023-07-27 00:11:59 +0600

    add:scaffold template

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,22 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: MainPage(),
       ),
     );
+
+class MainPage extends StatefulWidget {
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.blue.shade900,
+        appBar: AppBar(
+            backgroundColor: Colors.blue.shade800,
+            title: Text("Ask Me Anything")));
+  }
+}
